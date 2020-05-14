96b4da81bcfe623a4f791cf36137d231f45bf45a
carrieholt <Carrie.Holt@dfo-mpo.gc.ca>
2020-05-14 09:46:15 -0700

    added Roxygen file for example data

---
+++ R/Module_exampleData.R
@@ -0,0 +1,19 @@
+#' ExampleData
+#'
+#' Multistock time-series of abundances, labeled "Stock1", "Stock2" and "Stock3"
+#' 40 years of data with an NA
+#'
+#' @docType data
+#'
+#' @usage data(ExampleData)
+#'
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(grav)
+#' times <- attr(grav, "time")
+#' phe <- grav$pheno
+#' \donttest{iplotCurves(phe, times)}
+"ExampleData"
