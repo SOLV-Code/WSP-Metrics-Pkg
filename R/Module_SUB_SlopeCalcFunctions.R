fe7252e01e4e8e68492ba07afe3e10bb2cd55fa5
carrieholt <Carrie.Holt@dfo-mpo.gc.ca>
2020-05-15 22:18:39 -0700

    removed NA constraint from per.change.mod.fast

--- R/Module_SUB_SlopeCalcFunctions.R
+++ R/Module_SUB_SlopeCalcFunctions.R
@@ -13,7 +13,7 @@
 # MODIFICATIONS: CHANGED TO SINGLE INPUT OBJECT (to use with apply, by) , ADDED NA HANDLING
 
 per.change<-function(data.in,na.rm=TRUE){
-		
+
 	if(na.rm){data.in <- na.omit(data.in)}
 	n<-dim(data.in)[[1]]
 	lm<-lm(data.in[,"spn"]~data.in[,"yr"])
@@ -34,7 +34,7 @@ per.change.mod<-function(vec.in,na.rm=TRUE){
 	return(pchange)
 }
 
-# modified version above, further modified for speed 
+# modified version above, further modified for speed
 # .lm.fit is almost 2 orders of magnitude faster than lm
 # (i.e. 70 times faster)
 # use index values instead of years to simplify pchange
@@ -45,23 +45,23 @@ per.change.mod<-function(vec.in,na.rm=TRUE){
 per.change.mod.fast<-function(vec.in,na.rm=TRUE){
 	#print("starting per.change")
 	if(na.rm){vec.in <- na.omit(vec.in)} # NEED TO DISCUSS THIS
-	
+
 	# if no NA  (left) in input
-	if(sum(is.na(vec.in)) == 0 ){	
+	#if(sum(is.na(vec.in)) == 0 ){
 	n<-length(vec.in)
 	yrs <- 1:n
 	lm.coeff <- .lm.fit(cbind(1,yrs),vec.in)$coefficients # uses model matrix that is usually created inside lm()
 	#print(lm.coeff)
 	pchange <- (exp(lm.coeff[1]+lm.coeff[2]*n) -  exp(lm.coeff[1]+lm.coeff[2])) / exp(lm.coeff[1]+lm.coeff[2]) *100
-	#print(pchange)	
-	}
-	
-	if(sum(is.na(vec.in)) >0 ){	pchange <- NA }
-	
-	
+	#print(pchange)
+	#}
+
+	#if(sum(is.na(vec.in)) >0 ){	pchange <- NA }
+
+
 	return(pchange)
-	
-	
+
+
 }
 
 
