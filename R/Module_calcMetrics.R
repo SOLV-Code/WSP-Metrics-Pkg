9366b5372add31a2897e33fb12ec12280597d336
SOLV-Code <30556192+SOLV-Code@users.noreply.github.com>
2024-08-26 16:58:07 -0700

    fixed variable name in calcMetrics()

--- R/Module_calcMetrics.R
+++ R/Module_calcMetrics.R
@@ -222,7 +222,7 @@ if(sum(!is.na(trend.vec)) >= length(trend.vec/2) ){na.skip.use <- FALSE}
 
 
   inf.idx <- !is.finite(trend.vec)
-  zero.idx <- vec.use == 0
+  zero.idx <- trend.vec == 0
   fix.idx <- inf.idx | zero.idx
   fix.idx[is.na(fix.idx)] <- FALSE
   trend.vec[fix.idx] <- log(runif(sum(fix.idx,na.rm = TRUE),0.00000001, min(trend.vec[!inf.idx],na.rm = TRUE)/2))
